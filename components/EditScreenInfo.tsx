4938155202eae6943fc064830aaa07284bac331d
dLars99 <davidplarsen@gmail.com>
2022-12-20 22:09:46 -0600

    Redo app with TS template

--- components/EditScreenInfo.tsx
+++ components/EditScreenInfo.tsx
@@ -1,42 +1,70 @@
-import * as WebBrowser from 'expo-web-browser';
-import { StyleSheet, TouchableOpacity } from 'react-native';
+import * as WebBrowser from "expo-web-browser";
+import { useState } from "react";
+import { Button, StyleSheet, TouchableOpacity } from "react-native";
 
-import Colors from '../constants/Colors';
-import { MonoText } from './StyledText';
-import { Text, View } from './Themed';
+import Colors from "../constants/Colors";
+import { ViewData } from "../screens/TabTwoScreen";
+import { MonoText } from "./StyledText";
+import { Text, View } from "./Themed";
 
-export default function EditScreenInfo({ path }: { path: string }) {
+export default function EditScreenInfo({
+  path,
+  data,
+}: {
+  path: string;
+  data: ViewData;
+}) {
+  const [count, setCount] = useState<number>(0);
+
   return (
     <View>
       <View style={styles.getStartedContainer}>
         <Text
           style={styles.getStartedText}
           lightColor="rgba(0,0,0,0.8)"
-          darkColor="rgba(255,255,255,0.8)">
+          darkColor="rgba(255,255,255,0.8)"
+        >
           Open up the code for this screen:
         </Text>
 
         <View
           style={[styles.codeHighlightContainer, styles.homeScreenFilename]}
           darkColor="rgba(255,255,255,0.05)"
-          lightColor="rgba(0,0,0,0.05)">
+          lightColor="rgba(0,0,0,0.05)"
+        >
           <MonoText>{path}</MonoText>
         </View>
 
         <Text
           style={styles.getStartedText}
           lightColor="rgba(0,0,0,0.8)"
-          darkColor="rgba(255,255,255,0.8)">
-          Change any of the text, save the file, and your app will automatically update.
+          darkColor="rgba(255,255,255,0.8)"
+        >
+          {data.text}
+        </Text>
+      </View>
+
+      <View style={styles.shakeIt}>
+        <Text>Here is a button for no reason</Text>
+        <Button onPress={() => setCount(count + 1)} title="Press me!" />
+        <Text>
+          {count === 0
+            ? "You have not pressed this button."
+            : `You have pressed this button ${count} times`}
         </Text>
       </View>
 
       <View style={styles.helpContainer}>
         <TouchableOpacity onPress={handleHelpPress} style={styles.helpLink}>
           <Text style={styles.helpLinkText} lightColor={Colors.light.tint}>
-            Tap here if your app doesn't automatically update after making changes
+            Tap here if your app doesn't automatically update after making
+            changes
           </Text>
         </TouchableOpacity>
+      </View>
+
+      <View style={styles.shakeIt}>
+        <Text>When in doubt, shake and restart!</Text>
       </View>
     </View>
   );
@@ -44,13 +72,13 @@ export default function EditScreenInfo({ path }: { path: string }) {
 
 function handleHelpPress() {
   WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/get-started/create-a-new-app/#opening-the-app-on-your-phonetablet'
+    "https://docs.expo.io/get-started/create-a-new-app/#opening-the-app-on-your-phonetablet"
   );
 }
 
 const styles = StyleSheet.create({
   getStartedContainer: {
-    alignItems: 'center',
+    alignItems: "center",
     marginHorizontal: 50,
   },
   homeScreenFilename: {
@@ -63,17 +91,20 @@ const styles = StyleSheet.create({
   getStartedText: {
     fontSize: 17,
     lineHeight: 24,
-    textAlign: 'center',
+    textAlign: "center",
   },
   helpContainer: {
     marginTop: 15,
     marginHorizontal: 20,
-    alignItems: 'center',
+    alignItems: "center",
   },
   helpLink: {
     paddingVertical: 15,
   },
   helpLinkText: {
-    textAlign: 'center',
+    textAlign: "center",
+  },
+  shakeIt: {
+    alignItems: "center",
   },
 });
